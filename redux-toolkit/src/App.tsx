ef88b91f8571495d6c19ccc475e6dd702f9c7568
Van trong <trongdn2405@gmail.com>
2024-06-25 16:55:25 +0700

    Init summarize commits

---
+++ redux-toolkit/src/App.tsx
@@ -0,0 +1,13 @@
+import logo from './logo.svg'
+
+function App() {
+  return (
+    <div className='App'>
+      <a className='App-link text-[red]' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
+        Learn React
+      </a>
+    </div>
+  )
+}
+
+export default App
